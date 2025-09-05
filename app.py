69383e81f2fbfe94079a490eb1ba29bac34ae867
vinay3551 <54929845+vinay3551@users.noreply.github.com>
2025-09-05 17:55:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ import os
 
 app = Flask(__name__)
 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
+
 STUDENTS_FILE = "students.xlsx"
 REDEMPTIONS_FILE = "redemptions.xlsx"
 
