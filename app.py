743edddc49c64ef150c1fcd24aedc0537ca646b2
vinay3551 <54929845+vinay3551@users.noreply.github.com>
2025-09-06 09:37:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -112,5 +112,5 @@ def download_redemptions():
         return f"Error downloading file: {e}"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    #app.run(debug=True)
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
